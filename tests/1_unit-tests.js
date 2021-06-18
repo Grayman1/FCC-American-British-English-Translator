58cdea6fa76e734179878535a78a700609783b30
Grayman1 <54072430+Grayman1@users.noreply.github.com>
2021-06-18 15:04:20 +0000

    Update readme.md with unit and functional tests. Create templates for unit tests and functional tests.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,4 +5,259 @@ const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
 
+  suite('Tranlation tests', () => {
+// Unit Test #1- Translate
+    test('#1 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #2- Translate
+    test('#2 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #3- Translate
+    test('#3 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #4- Translate
+    test('#4 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #5- Translate
+    test('#5 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #6- Translate
+    test('#6 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #7- Translate
+    test('#7 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #8- Translate
+    test('#8 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #9- Translate
+    test('#9 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #10- Translate
+    test('#10 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #11- Translate
+    test('#11 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #12- Translate
+    test('#12 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #13- Translate
+    test('#13 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #14- Translate
+    test('#14 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #15- Translate
+    test('#15 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #16- Translate
+    test('#16 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #17- Translate
+    test('#17 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #18- Translate
+    test('#18 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #19- Translate
+    test('#19 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #20- Translate
+    test('#20 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #21- Translate
+    test('#21 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #22- Translate
+    test('#22 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #23- Translate
+    test('#23 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+// Unit Test #24- Translate
+    test('#24 - Translate', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+  })
+
+
+
+  suite('Highlight tests', () => {
+// Unit Test #25- Highlight
+    test('#25 - Highlight', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #26- Highlight
+    test('#26 - Highlight', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #27- Highlight
+    test('#27 - Highlight', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #29- Highlight
+    test('#29 - Highlight', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+// Unit Test #30- Highlight
+    test('#30 - Highlight', (done) => {
+      let validPuzzle = puzzlesAndSolutions[0][0];
+      let completePuzzle = puzzlesAndSolutions[0][1];
+      assert.equal(solver.solve(validPuzzle), completePuzzle);
+      done();
+      })
+
+
+
+  })
+  
+
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,4 +9,118 @@ let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
 
+  //Functional Test #1-
+/*
+  test('#1-', (done) => {
+   chai
+    .request(server)
+    .post('/api/solve')
+    .send({puzzle: validPuzzle})
+    .end((err, res) => {        
+      assert.equal(res.status, 200);
+      assert.isObject(res.body);
+      assert.property(res.body, 'solution');
+      let solvedPuzzle = puzzlesAndSolutions[1][1];
+      assert.equal(res.body.solution, solvedPuzzle);    
+      done();
+    })
+  })
+*/
+
+  //Functional Test #2-
+/*
+  test('#2-', (done) => {
+   chai
+    .request(server)
+    .post('/api/solve')
+    .send({puzzle: validPuzzle})
+    .end((err, res) => {        
+      assert.equal(res.status, 200);
+      assert.isObject(res.body);
+      assert.property(res.body, 'solution');
+      let solvedPuzzle = puzzlesAndSolutions[1][1];
+      assert.equal(res.body.solution, solvedPuzzle);    
+      done();
+    })
+  })
+*/
+
+  //Functional Test #3-
+/*
+  test('#3-', (done) => {
+   chai
+    .request(server)
+    .post('/api/solve')
+    .send({puzzle: validPuzzle})
+    .end((err, res) => {        
+      assert.equal(res.status, 200);
+      assert.isObject(res.body);
+      assert.property(res.body, 'solution');
+      let solvedPuzzle = puzzlesAndSolutions[1][1];
+      assert.equal(res.body.solution, solvedPuzzle);    
+      done();
+    })
+  })
+*/
+
+  //Functional Test #4-
+/*
+  test('#4-', (done) => {
+   chai
+    .request(server)
+    .post('/api/solve')
+    .send({puzzle: validPuzzle})
+    .end((err, res) => {        
+      assert.equal(res.status, 200);
+      assert.isObject(res.body);
+      assert.property(res.body, 'solution');
+      let solvedPuzzle = puzzlesAndSolutions[1][1];
+      assert.equal(res.body.solution, solvedPuzzle);    
+      done();
+    })
+  })
+*/
+
+  //Functional Test #5-
+/*
+  test('#5-', (done) => {
+   chai
+    .request(server)
+    .post('/api/solve')
+    .send({puzzle: validPuzzle})
+    .end((err, res) => {        
+      assert.equal(res.status, 200);
+      assert.isObject(res.body);
+      assert.property(res.body, 'solution');
+      let solvedPuzzle = puzzlesAndSolutions[1][1];
+      assert.equal(res.body.solution, solvedPuzzle);    
+      done();
+    })
+  })
+*/
+
+
+  //Functional Test #6-
+/*
+  test('#6-', (done) => {
+   chai
+    .request(server)
+    .post('/api/solve')
+    .send({puzzle: validPuzzle})
+    .end((err, res) => {        
+      assert.equal(res.status, 200);
+      assert.isObject(res.body);
+      assert.property(res.body, 'solution');
+      let solvedPuzzle = puzzlesAndSolutions[1][1];
+      assert.equal(res.body.solution, solvedPuzzle);    
+      done();
+    })
+  })
+*/
+
+
+
+
+  
+
 });
